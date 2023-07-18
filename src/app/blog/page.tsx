a6a122feca40181bf887d3cf61656837560a0856
gabiliz <gabrielalizmoreira@gmail.com>
2023-07-18 16:22:27 -0300

    wip: remove useState from blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
-import { FunctionComponent, useEffect, useState } from 'react';
+import { FunctionComponent } from 'react';
 
 type Post = {
 	title: string;
@@ -16,15 +16,9 @@ type Props = {
 
 const Blog: FunctionComponent<Props> = async () => {
   const server = process.env.SERVER;
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const postsData = await fetch(`${server}/api/content`).then((res) => res.json());
-      setPosts(postsData);
-    };
-    fetchPosts();
-  }, []);
+	const posts: Post[] = await fetch(`${server}/api/content`).then(
+		(res) => res.json()
+	);
 
   return (
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-10 lg:pt-24'>
